d5ab42061534559cc3fb2e23a3213133bed12485
naimur20163@gmail.come <naimur202163@gmail.comm>
2022-06-05 22:49:36 +0600

    python

---
+++ list.py
@@ -0,0 +1,13 @@
+for x in range(6):
+    print(x)
+else:
+    print("Finally finished!")
+
+
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banna", "chery"]
+
+
+for x in adj:
+    for y in fruits:
+        print(x, y)
