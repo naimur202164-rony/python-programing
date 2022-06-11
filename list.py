a36d52d5e30d57840ab7897a4bd8081be88b71e2
naimur20163@gmail.come <naimur202163@gmail.comm>
2022-06-11 14:41:13 +0600

    nila

--- list.py
+++ list.py
@@ -11,3 +11,43 @@ fruits = ["apple", "banna", "chery"]
 for x in adj:
     for y in fruits:
         print(x, y)
+
+
+# Lenth of Lists
+
+
+rony = ["Naimur", "Rony", "Rohima"]
+
+
+print(len(rony))
+# Type
+
+
+print(type(rony))
+
+# Acces In to List
+
+
+print(rony[1])
+
+
+# print(rony(-1))
+
+
+# Change list item;
+
+
+rony[1] = "Fuker"
+
+print(rony)
+
+
+# Add elements to Lists
+
+
+rony.append("Added")
+print(rony)
+
+
+rony.insert(2, "GGGGGGGG")
+print(rony)
